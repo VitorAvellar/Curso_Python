0ceec0ce36144669594c01f59e6ebcd284785eb7
Vitor Avellar <vitor07sa@gmail.com>
2024-05-10 11:50:17 -0300

    Feito no dia 10-05

---
+++ Curso Python/Secao-6-Controle-de-Fluxo/For-loop-SeparetedStringd.py
@@ -0,0 +1,7 @@
+# MODIFICAR DE FANTATISCO PARA F A N T A S T I C O
+
+palavra = 'FANTASTICO'
+
+for space in palavra:
+    print(f'{space}', end=' ')#end adiciona oque estiver dentro das aspas depois de um index
+    
