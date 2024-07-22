337d7ee8134e294c272da157e7609a2ced9c8d3e
Vitor Avellar <vitor07sa@gmail.com>
2024-07-22 19:29:51 -0300

    novas_aulas

---
+++ Curso Python/Desafios/desafio14/loopComBreak.py
@@ -0,0 +1,11 @@
+print('loop com o Break: ')
+for i in range (1, 11):
+    if i == 6:
+        break
+    print(i)
+    
+print('\nloop com o Continue: ')
+for x in range (1, 11):
+    if x == 5:
+        continue
+    print(x)
---
+++ Curso Python/Secao-7_Funcoes/4.Argumento_Default_e_nãoDefault.py
@@ -0,0 +1,11 @@
+#Default -> aqule que define o valor do paramatro
+#não Default -> aquele que não define o valor do parametro
+
+
+
+
+def boas_vindas(quantidade, nome='vitor2'):
+     print(f'Olá {nome}')
+     print(f'Temos {str(quantidade)} laptops em estoque')
+    
+boas_vindas(6)
